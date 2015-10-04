f651fc0e5e54aacaa75b4af42e6c2daae3dcba47
Anh Vo <anhvofrcaus@gmail.com>
2015-10-03 22:56:27 -0700

    create debug branch

--- hello_world.adb
+++ hello_world.adb
@@ -5,5 +5,6 @@ procedure Hello_World is
 begin
    Put_Line ("Welcome to Ada world");
    Put_Line ("Ada provide better syntactic check - develop branch");
+   Put_Line ("Ada provides runtime check - debug branch");
 end Hello_World;
 
