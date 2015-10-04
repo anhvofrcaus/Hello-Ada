c10ea47ae7d9670fac4652038a200ee91a660314
Anh Vo <anhvofrcaus@gmail.com>
2015-10-04 10:36:01 -0700

    fix in bug_1 branch

--- hello_world.adb
+++ hello_world.adb
@@ -6,5 +6,6 @@ begin
    Put_Line ("Welcome to Ada world");
    Put_Line ("Ada provide better syntactic check - develop branch");
    Put_Line ("Ada provides runtime check - debug branch");
+   Put_Line ("Ada provides runtime check - debug_1 branch");
 end Hello_World;
 
